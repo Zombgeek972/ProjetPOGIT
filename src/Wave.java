d8a7c3b0ea13798c05109481ff789d07d5052139
Zombgeek972 <lactelo2@gmail.com>
2024-12-15 16:13:56 +0100

    wave à peu près rempli

--- src/Wave.java
+++ src/Wave.java
@@ -1,6 +1,12 @@
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.List;
 
 public class Wave {
     private List<Ennemis> lstEnnemis;
     private List<Double> lstTimeToSpawn;
+
+    public Wave(String nomFichier){
+        Path path = FileSystems.getDefault().getPath("ressources/waves", nomFichier);
+    }
 }
