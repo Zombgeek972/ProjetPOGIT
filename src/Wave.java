c90d0fcc3fd12d328aec26658de2e367890fbdd9
Zombgeek972 <lactelo2@gmail.com>
2024-12-15 16:14:22 +0100

    wave presque rempli

--- src/Wave.java
+++ src/Wave.java
@@ -1,6 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Wave {
     private List<Ennemis> lstEnnemis;
@@ -8,5 +14,27 @@ public class Wave {
 
     public Wave(String nomFichier){
         Path path = FileSystems.getDefault().getPath("ressources/waves", nomFichier);
+        Queue<String> queue = new LinkedList<String>();
+        //enregistrement du fichier dans une queue contenant toutes les lignes
+        try (BufferedReader readerer = Files.newBufferedReader(path)) {
+            BufferedReader reader = Files.newBufferedReader(path);
+            String line = readerer.readLine();
+
+            while (((line = reader.readLine()) != null) ) {
+                queue.add(line);
+            }
+            reader.close();
+        } 
+        catch (IOException e) {
+            System.out.println(e);
+        }
+        lstEnnemis = new ArrayList<>();
+        lstTimeToSpawn = new ArrayList<>();
+        int i = 0;
+        while (queue.isEmpty()) {
+            String line = queue.remove();
+            String[] linepart = queue.split("|") ;
+            i++;
+        }
     }
 }
